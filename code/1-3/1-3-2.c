d17d1e4ae605bbc669685d638ea33ce552b9527f
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-04-01 15:51:21 +0900

    Update 1-3-2.c

--- code/1-3/1-3-2.c
+++ code/1-3/1-3-2.c
@@ -3,13 +3,13 @@
 #include "util.h"
 
 void logger_global(char* message) {
-    staitc int counter = 0;
+    static int counter = 0;
     printf("log %2d: %s\n", counter++, message);
 }
 
 void task2() {
     for (int i=0; i<4; i++) {
-        logger_static_intline("task2");
+        logger_static_inline("task2");
         logger_global("task2");
     }
 }
