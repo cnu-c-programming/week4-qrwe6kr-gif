3efd4a5f958ea88775efc231b6c6314cf4975a44
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-04-01 15:55:03 +0900

    Update util.h

--- code/1-3/util.h
+++ code/1-3/util.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-static inline void logger(char* message) {
+static inline void logger_static_inline(char* message) {
     static int counter = 0;
     printf("log %2d: %s\n", counter++, message);
 }
